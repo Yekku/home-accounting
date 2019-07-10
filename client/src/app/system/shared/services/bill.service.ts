b6947cbbca39deef054fefcbb6b676877d9b1d5d
Yekku <33233439+Yekku@users.noreply.github.com>
2019-07-10 08:54:21 +0300

    update bill service

--- client/src/app/system/shared/services/bill.service.ts
+++ client/src/app/system/shared/services/bill.service.ts
@@ -7,7 +7,7 @@ import { HttpService } from 'app/shared/services/http.service';
 
 @Injectable()
 export class BillService {
-  constructor(private HttpService: HttpService, public http: Http) {}
+  constructor(private HttpService: HttpService, private http: Http) {}
 
   addBill(bill: Bill): Observable<Bill> {
     return this.HttpService.createPost('bills', bill);
@@ -21,8 +21,8 @@ export class BillService {
     return this.HttpService.createPut(`bills/${_id}`, bill);
   }
 
-  getCurrency(): Observable<any> {
-    return this.HttpService.createGet(
+  getCurrency() {
+    return this.http.get(
         `http://data.fixer.io/api/latest?access_key=50f8bded1c8cc758592581b654bb7126`,
       )
       .map((response: Response) => response.json());
